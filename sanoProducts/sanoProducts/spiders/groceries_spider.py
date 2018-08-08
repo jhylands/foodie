3282575610af7da4b8d2e6d40da440647c5acb71
jhylands <jhh521@york.ac.uk>
2018-08-08 14:42:18 +0100

    A functional scraper

--- sanoProducts/sanoProducts/spiders/groceries_spider.py
+++ sanoProducts/sanoProducts/spiders/groceries_spider.py
@@ -1,13 +1,21 @@
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor as getLinks
 from scrapy.linkextractors import LinkExtractor
 
-class GrocerySpider(CrawlSpider):
-    name='groceries'
-    allowed_domains =['https://sainsburys.co.uk']
-    start_urls =  ['https://www.sainsburys.co.uk/shop/gb/groceries/']
-    rules = (Rule(LinkExtractor(allow='\/groceries\/'),callback='parse'),)
 
-    def parse(self,resposne):
-        pass
+class GrocerySpider(scrapy.Spider):
+    name='groceries'
+    allowed_domains =['https://sainsburys.co.uk','sainsburys.co.uk']
+    start_urls =  ['https://www.sainsburys.co.uk/shop/gb/groceries/']
+
+    def parse(self,response):
+        productpage = response.css('div.productTitleDescriptionContainer h1::text').extract() 
+        if not productpage:
+#allow='\/groceries\/'
+            links = getLinks().extract_links(response)
+            for link in links:
+                yield scrapy.Request(link.url, callback=self.parse)
+        
+        yield {'content':productpage}
 
